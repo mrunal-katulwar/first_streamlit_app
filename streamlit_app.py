b8f5dc700dff8da858e13c5843ab96e0a0d929eb
Mrunal Katulwar <81948671+mrunal-katulwar@users.noreply.github.com>
2022-05-01 00:00:24 +0530

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,10 @@
 import streamlit
-streamlit.title('My parents new healthy Diner')
+streamlit.title('My Mom\'s new healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Bluberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
+
+
